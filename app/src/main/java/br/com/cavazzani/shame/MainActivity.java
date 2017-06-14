3c01a294fb84b2bc1a38c815ed63cee6e372d5fa
cavazzanileonardo <cavazzanileonardo@gmail.com>
2017-06-13 22:57:28 -0300

    shake and sound

--- app/src/main/java/br/com/cavazzani/shame/MainActivity.java
+++ app/src/main/java/br/com/cavazzani/shame/MainActivity.java
@@ -1,13 +1,22 @@
 package br.com.cavazzani.shame;
 
+import android.hardware.SensorListener;
+import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorListener {
+
+    private SensorManager sensorMgr;
+    private static final int SHAKE_THRESHOLD = 800;
+    private long lastUpdate; private float x; private float y; private float z; private float last_x; private float last_z; private float last_y;
+    MediaPlayer mp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,12 +24,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ImageButton one = (ImageButton) this.findViewById(R.id.shame);
-        final MediaPlayer mp = MediaPlayer.create(this, R.raw.shame);
+        sensorMgr = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        sensorMgr.registerListener(this,
+                SensorManager.SENSOR_ACCELEROMETER,
+                SensorManager.SENSOR_DELAY_GAME);
+
+        mp = MediaPlayer.create(this, R.raw.shame);
         one.setOnClickListener(new View.OnClickListener(){
 
             public void onClick(View v) {
                 mp.start();
             }
         });
+    }
+
+    @Override
+    public void onSensorChanged(int sensor, float[] values) {
+        if (sensor == SensorManager.SENSOR_ACCELEROMETER) {
+            long curTime = System.currentTimeMillis();
+            // only allow one update every 100ms.
+            if ((curTime - lastUpdate) > 100) {
+                long diffTime = (curTime - lastUpdate);
+                lastUpdate = curTime;
+
+                x = values[SensorManager.DATA_X];
+                y = values[SensorManager.DATA_Y];
+                z = values[SensorManager.DATA_Z];
+
+                float speed = Math.abs(x+y+z - last_x - last_y - last_z) / diffTime * 10000;
+
+                if (speed > SHAKE_THRESHOLD) {
+                    Log.d("sensor", "shake detected w/ speed: " + speed);
+                    mp.start();
+                }
+                last_x = x;
+                last_y = y;
+                last_z = z;
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(int sensor, int accuracy) {
+
     }
 }

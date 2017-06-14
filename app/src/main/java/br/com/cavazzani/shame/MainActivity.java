b5ac3d59fa9950775d61442a124d63f29af181f5
cavazzanileonardo <cavazzanileonardo@gmail.com>
2017-06-13 22:51:21 -0300

    remove actionbar

--- app/src/main/java/br/com/cavazzani/shame/MainActivity.java
+++ app/src/main/java/br/com/cavazzani/shame/MainActivity.java
@@ -1,7 +1,11 @@
 package br.com.cavazzani.shame;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton one = (ImageButton) this.findViewById(R.id.shame);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.shame);
+        one.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v) {
+                mp.start();
+            }
+        });
     }
 }
